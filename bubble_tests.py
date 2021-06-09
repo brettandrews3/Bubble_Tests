c6c89f94de98e6013755f8522737068e4b6a2d9d
Brett Andrews <brettandrews3@gmail.com>
2021-06-09 16:45:20 -0500

    Wrote for loop to print 'Bubble solution...' statements for the list. Added TODO comment for next steps.

--- bubble_tests.py
+++ bubble_tests.py
@@ -4,7 +4,7 @@
 scores = [60, 50, 60, 58, 54, 54,
             58, 50, 52, 54, 48, 69,
             34, 55, 51, 52, 44, 51,
-            69, 64, 66, 555, 52, 61,
+            69, 64, 66, 55, 52, 61,
             46, 31, 57, 52, 44, 18,
             41, 53, 55, 61, 51, 44]
 
@@ -12,15 +12,23 @@ scores = [60, 50, 60, 58, 54, 54,
 """
 print('Bubble solution #0 score:', scores[0])
 print('Bubble solution #1 score:', scores[1])
-print('Bubble solution #2 score:', scores[2], '\n')
 """
 
-# While loop to print 'Bubble solution #x score: ':
+"""
+# While loop to print 'Bubble solution #i score: i':
 i = 0
 length = len(scores)
 while i < length:
     print('Bubble solution #' + str(i), 'score:', scores[i])
     i = i + 1
+"""
 
-# Return to print calculated averages, scores[i] for highest values
+# For loop version of the same statement:
+i = 0
+for score in scores:
+    output = 'Bubble solution #' + str(i) + ': ' + str(score)
+    print(output)
+    i = i + 1
+
+#TODO Return to print calculated averages, scores[i] for highest values
 print('Bubble tests:', len(scores))
