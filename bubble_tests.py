60288e3d3b828062e77ce78f4d3a7b04de368f6a
Brett Andrews <brettandrews3@gmail.com>
2021-06-09 15:41:35 -0500

    1st commit. Next step: write a while loop to print 'Bubble solution #x' for each item in scores[].

---
+++ bubble_tests.py
@@ -0,0 +1,16 @@
+# Bubble Factory exercise
+
+# Scores = results from the bubble tests
+scores = [60, 50, 60, 58, 54, 54,
+            58, 50, 52, 54, 48, 69,
+            34, 55, 51, 52, 44, 51,
+            69, 64, 66, 555, 52, 61,
+            46, 31, 57, 52, 44, 18,
+            41, 53, 55, 61, 51, 44]
+
+# Here's a few print statements that pull from the list:
+print('Bubble solution #0 score:', scores[0])
+print('Bubble solution #1 score:', scores[1])
+print('Bubble solution #2 score:', scores[2], '\n')
+
+print('Bubble tests:', len(scores))
