0af32b9e4a68da576b2d6a7db0b43d485cc6cc2f
Brett Andrews <brettandrews3@gmail.com>
2021-06-16 16:49:08 -0500

    Added costs list and for loop to find the most_effective bubble test according to high score and low cost. My print() at ln 42 isn't working for some reason.

--- bubble_tests.py
+++ bubble_tests.py
@@ -1,12 +1,20 @@
 # Bubble Factory exercise
 
-# Scores = results from the bubble tests
+# scores = results from the bubble tests
+# costs = cost of the test in the parallel list scores[]
 scores = [60, 50, 60, 58, 54, 54,
             58, 50, 52, 54, 48, 69,
             34, 55, 51, 52, 44, 51,
             69, 64, 66, 55, 52, 61,
             46, 31, 57, 52, 44, 18,
             41, 53, 55, 61, 51, 44]
+
+costs = [.25, .27, .25, .25, .25, .25,
+            .33, .31, .25, .29, .27, .22,
+            .31, .25, .25, .33, .21, .25,
+            .25, .25, .28, .25, .24, .22,
+            .20, .25, .30, .25, .24, .25,
+            .25, .25, .27, .25, .26, .29]
 
 # Iterates through list 'scores' once to find the high_score:
 high_score = 0
@@ -21,10 +29,20 @@ for i in range(length):
     if high_score == scores[i]:
         best_solutions.append(i)
 
-# Print the results: number of tests, high_score, best_solutions:
+# For loop determines most effective bubble test by looking for
+# lowest cost and high_score to produce most_effective:
+cost = 100.0
+most_effective = 0
+for i in range(length):
+    if scores[i] == high_score and costs[i] < cost:
+        most_effective = i
+        cost = costs[[i]
+
+# Print the results: number of tests, high_score, best_solutions, most_effective:
 print('Bubble tests:', length)
 print('Highest bubble test score:', high_score)
 print('Tests with the highest score:', best_solutions)
+print('Bubble test,' most_effective, 'is the best one with a cost of', costs[most_effective])
 
 # Write while loop to iterate through 'scores' list...:
 """
